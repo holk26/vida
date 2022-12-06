30f669faf266d4095dd4f7572805e3f51056a31d
HOMERO CABRERA ARAQUE <homero.cabrera@telefonica.com>
2022-12-05 21:00:39 -0500

    Message

---
+++ pages/chispas/index.js
@@ -0,0 +1,20 @@
+import styles from '../../styles/Home.module.css'
+
+
+const Chispas = () => {
+    return(
+        <main className={styles.main}>
+        <h1 className={styles.title}>
+          Mensaje para la mujer mas linda del universo
+        </h1>
+        <h2>Cada momento, cada instante me enamoro mas de ti, de tu sonrisa de tu mirada de tu ser, todo de ti 
+            me tiene loco y me encanta, haces que los días sean increíbles. los momentos mas lindos y
+            especiales los he vivido contigo Diana Alejandra Pachon garzon <strong>Te amoooo</strong>
+        </h2>
+
+
+      </main>
+    );
+}
+
+export default Chispas;
---
+++ pages/products/index.js
@@ -0,0 +1,11 @@
+
+
+const Products = () => {
+    return(
+        <div>
+            <h1>Eres la mujer mas linda el mundo</h1>
+        </div>
+    );
+}
+
+export default Products;
