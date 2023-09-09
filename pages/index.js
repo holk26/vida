ab593ced1f6af7269a72b43d6a5fb3247739be1b
Homero Cabrera Araque <homero9726@gmail.com>
2023-09-09 11:27:09 -0500

    two

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by Vida{' '}
+          Powered by VidaMia{' '}
           <span className={styles.logo}>
             {/*<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*/}
           </span>
